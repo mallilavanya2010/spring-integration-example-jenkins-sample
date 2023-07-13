30e832b383fe0584fc10beab792faf6a155241c9
Mallikarjuna Vangiwaka <mallilavanya2010@gmail.com>
2023-07-13 12:16:59 -0400

    first commit

---
+++ src/main/java/com/example/demo/springintegrationexamplejenkins/SpringIntegrationExampleJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springintegrationexamplejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationExampleJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationExampleJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/springintegrationexamplejenkins/SpringIntegrationExampleJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springintegrationexamplejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationExampleJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
